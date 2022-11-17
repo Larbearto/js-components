57bb62c47837c83e6acb6f2bd603cf1e77b780da
SpiderBear <SpiderBear714@gmail.com>
2022-11-17 02:23:38 -0800

    added filters and dadjoke component

---
+++ 21-dad-jokes/app.js
@@ -0,0 +1,30 @@
+const url = 'https://icanhazdadjoke.com/'
+
+const jokeBtn = document.querySelector('.btn')
+const result = document.querySelector('.result')
+
+jokeBtn.addEventListener('click', () => {
+  fetchDadJoke()
+})
+
+const fetchDadJoke = async () => {
+  result.textContent = 'Loading...'
+  try {
+    const response = await fetch(url, {
+      headers: {
+        Accept: 'application/json',
+        'User-Agent': 'learning app'
+      }
+    })
+    if (!response.ok) {
+      throw new Error(' error')
+    }
+    const data = await response.json()
+    result.textContent = data.joke
+  } catch (error) {
+    console.log(error.message)
+    result.textContent = 'There was an error ...'
+  }
+}
+
+fetchDadJoke()
